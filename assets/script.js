dc4fb2f57455a0b588dd99e7f6ee4f7297e76492
Ted Slesinski <ted@slesinski.org>
2022-11-16 12:27:06 -0500

    Changed layout

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,7 @@
+var beers = document.querySelectorAll('section article');
+var row1 = beers.length / 2;
+var percent = 1 / row1 * 100;
 
+for (let beer of beers) {
+  beer.style.flexBasis = percent +"%";
+}
