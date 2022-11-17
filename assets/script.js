7471cdd2805054acf4191611e83cb9bacb6b44f5
Ted Slesinski <ted@slesinski.org>
2022-11-17 12:14:03 -0500

    Add random color to kiosk

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,12 @@
-var beers = document.querySelectorAll('section article');
-var row1 = beers.length / 2;
-var percent = 1 / row1 * 100;
+let beers = document.querySelectorAll('section article'),
+    row1 = beers.length / 2,
+    percent = 1 / row1 * 100;
 
 for (let beer of beers) {
   beer.style.flexBasis = percent +"%";
+}
+
+let items = document.querySelectorAll("article");
+for (let i = 0; i < items.length; i++) {
+  items[i].style.color = randomColor({ luminosity: "light" });
 }
