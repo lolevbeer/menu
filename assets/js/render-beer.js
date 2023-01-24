0227460e92d12bd1fc4a705f6a9e302b318aa28b
Ted Slesinski <ted@slesinski.org>
2023-01-24 17:10:51 -0500

    Fix coloring of regular page

--- assets/js/render-beer.js
+++ assets/js/render-beer.js
@@ -5,26 +5,21 @@ new Vue({
     beers: []
   },
   methods: {
-    loadData: function () {
-      fetch('https://lolev.beer/menu.json')
-        .then(response =>
-          response.json()
-        )
-        .then(data =>
-          this.beers = data
-        );
+    async loadData() {
+      const response = await fetch('https://lolev.beer/menu.json');
+      const data = await response.json();
+      this.beers = data;
+      this.$nextTick(this.changeColors);
     },
-    changeColors: function () {
+    changeColors() {
       let items = document.querySelectorAll('article');
+      console.log(items.length)
       for (let i = 0; i < items.length; i++) {
         items[i].style.color = randomColor({ luminosity: "light" });
       }
     }
   },
-  mounted: function () {
+  mounted() {
     this.loadData();
-    setInterval(function () {
-      this.changeColors();
-    }.bind(this), 5000);
   }
 });
