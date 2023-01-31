5341a229304553a20ce94e6c0ac7716740d2a5ac
Ted Slesinski <ted@slesinski.org>
2023-01-31 16:20:11 -0500

    First commit pwa

--- assets/js/render-beer.js
+++ assets/js/render-beer.js
@@ -1,3 +1,15 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/assets/js/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
 new Vue({
   delimiters: ['${', '}'],
   el: '#app',
---
+++ assets/js/sw.js
@@ -0,0 +1,51 @@
+// sw.js
+
+const CACHE_NAME = 'my-pwa-cache';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/assets/css/main.css',
+  '/assets/js/render-beer.js'
+];
+
+self.addEventListener('install', event => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then(
+          response => {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clone the response
+            let responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+  );
+});
